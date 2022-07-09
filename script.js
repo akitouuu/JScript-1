03187e82f83cf032fd5a8fc0e897366c09b4fda8
akitouuu <fixserials06@gmail.com>
2022-07-10 01:22:29 +0500

    JS1

---
+++ script.js
@@ -0,0 +1,29 @@
+var name = prompt('Введите имя:');
+console.log(name);
+
+var age = +prompt('Введите ваш возраст:');
+console.log(age);
+
+var plus = 10 + 9;
+var ask = +prompt('Вычислите: 10 + 9')
+console.log('10 + 9 = ' + plus + ' (Ваш ответ ' + ask + ')');
+
+var plus = 31 - 11;
+var ask = +prompt('Вычислите: 31 - 11')
+console.log('31 - 11 = ' + plus + ' (Ваш ответ ' + ask + ')');
+
+var plus = 5 * 6;
+var ask = +prompt('Вычислите: 5 * 6')
+console.log('5 * 6 = ' + plus + ' (Ваш ответ ' + ask + ')');
+
+var plus = 30 / 5;
+var ask = +prompt('Вычислите: 30 / 5')
+console.log('30 / 5 = ' + plus + ' (Ваш ответ ' + ask + ')');
+
+var plus = 10 % 3;
+var ask = +prompt('Вычислите: 10 % 3')
+console.log('10 % 3 = ' + plus + ' (Ваш ответ ' + ask + ')');
+
+alert('Перейдите в консоль что бы увидеть правильные ответы!')
+
+
